4ec95a9fbf14836e9cd99a8cb91a84ad2a1e2a15
Wilson Redd <wilsonredd@gmail.com>
2020-04-02 11:58:55 -0600

    Created responsive navigation bar

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <img class="logo" alt="Vue logo" src="./assets/logo.png">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/add">Add Friend</router-link> |
-      <router-link to="/update">Update My Info</router-link>
+      <div class="brand">
+        <img class="logo" alt="Faces logo" src="/images/logo.jpg">
+        <h1>Good With Names</h1>
+      </div>
+      <div class="links">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/add">Add Friend</router-link> |
+        <router-link to="/update">Update My Info</router-link>
+      </div>
     </div>
     <router-view/>
     <div id="footer">
@@ -14,24 +19,44 @@
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  min-height: 100vh;
+  padding: 0px 5px 0px 5px;
+}
+
+.logo {
+  height: 100px;
 }
 
 #nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  padding: 30px 10px 30px 10px;
 }
 
-#nav a {
+.brand h1 {
+  margin: 10px;
+}
+
+.links a {
   font-weight: bold;
+  font-size: 1.25em;
+  text-align: center;
+  text-decoration: none;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+.links a.router-link-exact-active {
   color: #42b983;
 }
 
@@ -42,6 +67,19 @@
 #footer a {
   text-align: center;
   text-decoration: none;
-  color: #2F4F4F;
+  color: #2c3e50;
 }
+
+@media only screen and (min-width: 1024px) {
+  #nav {
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .brand h1 {
+    margin: 5px 10px 10px 10px;
+  }
+}
+
 </style>
